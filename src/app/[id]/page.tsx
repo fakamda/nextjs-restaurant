4de2ade6f47a41a6a21bba00eae4c20167054ee5
fakamda <facundoelcardenal@gmail.com>
2024-01-26 23:42:28 -0300

    metadata

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,14 @@
 import api from "@/api";
 
+export async function generateMetadata({params: {id}}: {params: {id: string}}) {
+    const restaurant = await api.fetch(id);
+  
+    return {
+      title: `${restaurant.name} - Restaurancy`,
+      description: restaurant.description,
+    };
+  }
+
 export default async function RestaurantPage({ params: { id } }: { params: { id: string } }) {
   const restaurant = await api.fetch(id);
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,10 @@ import type {Metadata} from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Restaurancy",
-};
+  title: 'Restaurancy',
+  description: 'The best restaurants in the world',
+  keywords: ['restaurant', 'food', 'eat', 'dinner', 'lunch'],
+}
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
