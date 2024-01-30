9657d69612d92222aa45c6728184d55c114ac553
fakamda <facundoelcardenal@gmail.com>
2024-01-29 23:32:31 -0300

    revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 import api from "@/api";
 
+export const dynamic = "force-dynamic"
+
 export default async function Home() {
   const restaurants = await api.list();
 
